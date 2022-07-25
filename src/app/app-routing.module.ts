1dff17c5b81c95ab68d37b2f514ca35f83234a85
Igor <igor.borowski@capgemini.com>
2022-07-25 14:27:13 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: '**', component: PathNotFoundComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
