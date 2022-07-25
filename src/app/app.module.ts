852082efc2715f22db92d8e2d127ddbcb868224a
Igor <igor.borowski@capgemini.com>
2022-07-25 14:25:53 +0200

    create pathnotfound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
