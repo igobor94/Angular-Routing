700d213dd4b89dd86d823fa682ea1ab535360344
Igor <igor.borowski@capgemini.com>
2022-07-25 14:56:06 +0200

    create home comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    PathNotFoundComponent
+    PathNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
